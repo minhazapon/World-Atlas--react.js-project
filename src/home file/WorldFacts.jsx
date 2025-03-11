9d87bdcaa4eb0cc690640462dd7ebcc98ebe27eb
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-11 01:35:51 -0700

    added file

--- src/home file/WorldFacts.jsx
+++ src/home file/WorldFacts.jsx
@@ -8,7 +8,9 @@ function WorldFacts() {
             <div className="ml-16 mr-16 p-5 mb-10 mt-10">
 
                 <div>
-
+                    <div>
+                        <p className=" text-center text-white font-semibold text-4xl">Here is the interesting<br></br> facts we're proud</p>
+                    </div>
 
 
                 </div>
