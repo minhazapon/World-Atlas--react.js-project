8b860f6725b1ef3894c0b5b4a1d6fe652c5c2e5b
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-11 01:59:21 -0700

    added file

--- src/home file/Contact.jsx
+++ src/home file/Contact.jsx
@@ -1,7 +1,11 @@
-
-
+import { CiLocationArrow1 } from "react-icons/ci";
+import { FaPhone } from "react-icons/fa6";
+import { MdMarkEmailRead } from "react-icons/md";
 
 function Contact() {
+
+
+
     return (
         <div>
 
