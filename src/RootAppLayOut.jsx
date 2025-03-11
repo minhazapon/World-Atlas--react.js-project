2907f57b5369787c801e1965641cb62cebbcacc4
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-10 23:26:38 -0700

    added file

--- src/RootAppLayOut.jsx
+++ src/RootAppLayOut.jsx
@@ -1,10 +1,11 @@
+import { Outlet } from "react-router-dom"
+import Nav from "./Nav"
 
 function RootAppLayOut() {
-
-
     return (
         <div>
-
+            <Nav></Nav>
+            <Outlet></Outlet>
         </div>
     )
 }
